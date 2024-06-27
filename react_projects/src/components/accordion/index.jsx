5803cd544ea7517ae6ec9afd4ffaf895e1831459
Pinerealm <osalami09@gmail.com>
2024-06-27 13:55:58 +0300

    chore: Add multi-selection feature to Accordion component

--- react_projects/src/components/accordion/index.jsx
+++ react_projects/src/components/accordion/index.jsx
@@ -4,31 +4,50 @@ import './styles.css';
 
 export default function Accordion() {
   const [selected, setSelected] = useState(null);
+  const [multiSelect, setMultiSelect] = useState(false);
+  const [selectedItems, setSelectedItems] = useState([]);
 
   function handleSingleSelection(currentId) {
     console.log(currentId);
     setSelected(currentId === selected ? null : currentId);
   }
-  console.log(selected);
+
+  function handleMultiSelection(currentId) {
+    if (selectedItems.includes(currentId)) {
+      setSelectedItems(selectedItems.filter((id) => id !== currentId));
+    } else {
+      setSelectedItems([...selectedItems, currentId]);
+    }
+  }
+  console.log(selected, selectedItems);
 
   return (
     <div className="wrapper">
+      <h2>Accordion Component</h2>
+      <button onClick={() => setMultiSelect(!multiSelect)}>
+        Enable multi-selection
+      </button>
       <div className="accordion">
         {data && data.length > 0 ? (
           data.map((dataItem) => (
             <div className="item">
               <div
-                onClick={() => handleSingleSelection(dataItem.id)}
+                onClick={
+                  multiSelect
+                    ? () => handleMultiSelection(dataItem.id)
+                    : () => handleSingleSelection(dataItem.id)
+                }
                 className="title"
               >
                 <h3>{dataItem.question}</h3>
                 <span>+</span>
               </div>
-              {selected === dataItem.id && (
+              {(multiSelect && selectedItems.includes(dataItem.id)) ||
+              (!multiSelect && selected === dataItem.id) ? (
                 <div className="content">
                   <p>{dataItem.answer}</p>
                 </div>
-              )}
+              ) : null}
             </div>
           ))
         ) : (
